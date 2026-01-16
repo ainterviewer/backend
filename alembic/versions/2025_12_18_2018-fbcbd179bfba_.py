59da71952026233a515f65d05ef5c3dddcef8733
gaardhus <tobias_gaardhus@hotmail.com>
2026-01-16 17:05:06 +0100

    add alembic migrations

--- alembic/versions/2025_12_18_2018-fbcbd179bfba_.py
+++ alembic/versions/2025_12_18_2018-fbcbd179bfba_.py
@@ -1,10 +1,11 @@
 """empty message
 
 Revision ID: fbcbd179bfba
-Revises: 
+Revises:
 Create Date: 2025-12-18 20:18:23.425637
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -14,7 +15,7 @@ import app.db.types
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'fbcbd179bfba'
+revision: str = "fbcbd179bfba"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
